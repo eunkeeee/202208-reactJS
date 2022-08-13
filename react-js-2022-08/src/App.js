40de427b8bac7fa9dd90235a0e6e981251ede249
eunkeeee <eunkeeeee@gmail.com>
2022-08-13 14:49:52 +0900

    6.2 Deps: useEffect 완성편

--- react-js-2022-08/src/App.js
+++ react-js-2022-08/src/App.js
@@ -2,14 +2,32 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
-  const iRunOnlyOnce = () => {
-    console.log("CALL THE API....");
+  const onChange = (event) => {
+    setKeyword(event.target.value);
   };
-  useEffect(iRunOnlyOnce, []);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("i only run once.");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("i run when 'keyword changes.");
+    }
+  }, [keyword]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      ></input>
       <h1>{counter}</h1>
       <button onClick={onClick}>Click Me</button>
     </div>
