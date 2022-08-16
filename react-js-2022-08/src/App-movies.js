a8a7a99bf1884fde15bfb0b8e74922869b14b56d
eunkeeee <eunkeeeee@gmail.com>
2022-08-16 19:30:54 +0900

    7.4 Props 사용해서 component 옮겨냄

--- react-js-2022-08/src/App-movies.js
+++ react-js-2022-08/src/App-movies.js
@@ -1,20 +1,38 @@
 import { useState, useEffect } from "react";
+import Movie from "./Movie";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
-  }, []);
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => getMovies(), []);
   console.log("movies:", movies);
-  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              id={movie.id}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
